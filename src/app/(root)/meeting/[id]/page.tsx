88bb0faeee75e892504d7bd44abea94cd86ef2ac
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-01-02 21:37:14 +0000

    implemented getCallById hook and prepped Meetings page

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,37 @@
+'use client';
+
+import LoaderUI from '@/components/LoaderUI';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation';
+import { useState } from 'react';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import useGetCallById from '@/hooks/useGetCallById';
+
 const MeetingPage = () => {
-  return <div>Meeting Page under development...</div>;
+  const { id } = useParams();
+  const { isLoaded } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <LoaderUI />;
+
+  if (!call) {
+    return (
+      <div className='h-screen flex items-center justify-center'>
+        <p className='text-2xl font-semibold'>Meeting not found</p>
+      </div>
+    );
+  }
+
+  return (
+    <StreamCall call={call}>
+      <StreamTheme>
+        {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+      </StreamTheme>
+    </StreamCall>
+  );
 };
 
 export default MeetingPage;
